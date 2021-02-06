f938139ece4412fa6e0654b3929253f37ad5c7de
jv-js <73355744+jv-js@users.noreply.github.com>
2021-02-07 05:59:15 +0700

    Update add.js

--- add.js
+++ add.js
@@ -85,18 +85,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
 
 /*scrollTop*/
-$(window).scroll(function(){
-    if($(window).scrollTop() >= 10) {
-      $('.scroll-up').show();
-    } else {
-      $('.scroll-up').hide();
-    }
-  });
-  function page_scroll2top(){
-    $('html,body').animate({
-      scrollTop: 0
-      }, 'fast');
-  }
+
   /*menu-btn*/
 
 $(document).ready(function () {
